a87f2c4eae423958ea84b427203ef487a90b2104
Alice Yau <yau.alice1@gmail.com>
2022-01-18 16:00:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ function createParagraph() {
   document.body.appendChild(para);
 }
 
-const buttons = document.querySelectorAll('button');
+const buttonB= document.querySelectorAll('#button_B');
 
-for (const button of buttons) {
+for (const button of buttonB) {
   button.addEventListener('click', createParagraph);
 }
+
+const buttonA = document.querySelector('#button_A');
+const headingA = document.querySelector('#heading_A');
+
+buttonA.onclick = () => {
+  const name = prompt('Thanks for saying Hi! What is your name?');
+  alert(`Hello ${name}, nice to see you!`);
+  headingA.textContent = `Welcome ${name}`;
+}  
